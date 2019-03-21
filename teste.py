42e719664c4971b5699c15a1a3cd37517da26763
kamilaalexandre <47819508+kamilaalexandre@users.noreply.github.com>
2019-03-21 11:08:26 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,4 +1,21 @@
-a = 40
-b = 50
-if a>b:
-  print(a)
+from flask import Flask, render_template, request
+app = Flask(__name__, static_url_path='/static')
+
+lista = [("Joao","Rua 9", "3322-5152"),
+         ("Maria","Rua 11", "99982-7080"),
+         ("Eugenia", "Rua 13", "3028-5612")]
+@app.route("/", methods=['POST','GET'])
+def inicio():
+    return render_template("inicio.html", lista = lista)
+
+@app.route('/easter/')
+def easter():
+    return render_template('easter.html', lista = lista)
+@app.route('/exibir_mensagem/', methods=['POST','GET'])
+def exibir():
+    nome = request.form.get('nome')
+    print(nome)
+    return render_template('exibir_mensagem.html', lista = lista)
+
+if __name__ == "__main__":
+    app.run(use_reloader = True)
